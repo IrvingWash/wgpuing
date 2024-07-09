87d82b6b4ec4a90ec5a0e709c696dd628dcbe753
Washington Irving <iamwashingyou@gmail.com>
2024-07-09 22:28:49 +0300

    draw the triangle

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ struct State<'a> {
     window_size: winit::dpi::PhysicalSize<u32>,
     window: &'a Window,
     clear_color: wgpu::Color,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl<'a> State<'a> {
@@ -68,6 +69,56 @@ impl<'a> State<'a> {
             desired_maximum_frame_latency: 2,
         };
 
+        // 3. Load shaders
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("My shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        // 4. Create render pipeline layout
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("My pipeline layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        // 5. Create render pipeline
+        // Render pipeline describes what actions GPU must perform on data
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("My render pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // ------ - Don't render triangles that are not visible
+                cull_mode: Some(wgpu::Face::Back), // ---/
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
         State {
             window,
             surface,
@@ -76,6 +127,7 @@ impl<'a> State<'a> {
             surface_config,
             window_size,
             clear_color: wgpu::Color::BLACK,
+            render_pipeline,
         }
     }
 
@@ -122,20 +174,26 @@ impl<'a> State<'a> {
                 label: Some("My command encoder"),
             });
 
-        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("My render pass"),
             depth_stencil_attachment: None,
             occlusion_query_set: None,
             timestamp_writes: None,
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(self.clear_color),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
+            color_attachments: &[
+                // This is the 0 element. @location(0) in the shader tells to relate to this element
+                Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })
+            ],
         });
+
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.draw(0..3, 0..1); // @builtin(vertex_index) passes these values to the shader
 
         // encoder was mutably borrowed when creating `render_pass`
         drop(render_pass);
